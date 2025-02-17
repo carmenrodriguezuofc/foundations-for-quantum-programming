c985b9853328f9520fc73a70e7716cc9bbc32441
carmenrodriguezuofc <carmen.rodriguezapar@ucalgary.ca>
2025-02-17 10:10:03 -0700

    Update listcomprehension_practice.py

--- List_Comprehension/listcomprehension_practice.py
+++ List_Comprehension/listcomprehension_practice.py
@@ -1,13 +1,13 @@
 costs=[5,10,3,4]
 items = [0,1,0,1]
-
+vqlues=[-costs[i]*items[i] for i in [0,1,2,3] if items[i] >0]
+print(values)
 #practice 1: creating a list comprehension with the function zip and create a variable for the sum values
 #values= [-x*y for x, y in zip(costs,items) if y !=0]
-#totalcost = sum(values)
-#print(totalcost)
+#print(values)
 #Practice 2: create a list comprehension Using the zip function to combine the elements of both lists
 #values= sum([-x*y for x, y in zip(costs,items) if y !=0])
 #print(values)
 #practice 3: Create a list comprehension with the iteration i
-values= sum([-costs[i]*items[i] for i in [0,1,2,3] if i>0])
-print(values)
+#values= sum([-costs[i]*items[i] for i in [0,1,2,3] if i>0])
+#print(values)
