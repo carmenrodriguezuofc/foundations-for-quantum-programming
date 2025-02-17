56a3e3c2d67c2077d25b0c2d820f7017460e9bb2
carmenrodriguezuofc <carmen.rodriguezapar@ucalgary.ca>
2025-02-17 14:54:13 -0700

    Update function_practice.py

--- Function/function_practice.py
+++ Function/function_practice.py
@@ -1,1 +1,10 @@
-
+def grocery(food):
+    if food == 'tofu':
+        print("Tofu is cheap and has few calories")
+    elif food =='meat':
+        print(f"Meat has the most calories")
+    elif food =='salmon' or food=='white fish':
+        print("Seafood is expensive but is healthy")
+    else:
+        print("Chicken has fewer calories than meat")
+grocery("meat")
